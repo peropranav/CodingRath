da2b2b9aa297c0d0b665aaea9a7703a32d348dd9
peropranav <pranavkapoor99@gmail.com>
2018-02-12 22:00:26 +0530

    adding code editor

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ var problem=require('./routes/problem');
 app.set('views', path.join(__dirname, 'views'));
 app.use('/problem',problem);
 
-
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port,function () {
     console.log("listening at port 3000")
