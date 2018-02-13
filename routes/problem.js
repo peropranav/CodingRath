4c4cd45cb2d55fa2ec8b9506e5960c9d319cfe60
Pranav Kapoor <pranavkapoor99@gmail.com>
2018-02-13 10:51:51 +0530

    adding logo

--- routes/problem.js
+++ routes/problem.js
@@ -2,18 +2,56 @@
 var express=require('express');
 var app=express();
 const router = require('express').Router();
-const problem=require('../models/problem')
+const Problem=require('../models/Problem')
 
 router.get('/addcodingproblem', function(req, res, next) {
-        res.render('addcodingproblem', {  });
+    res.render('addcodingproblem', {  });
 });
 
 
 
 router.post('/addcodingproblem', function(req, res, next) {
-    console.log(req.body);
+    // console.log(req.body);
+    var ques_label = req.body.ques_label;
+    var   statement= req.body.statement;
+    var  constraint= req.body.constraint;
+    var  input_Format= req.body.input_Format;
+    var  output_Format= req.body.output_Format;
+    var  sample_Input= req.body.sample_Input;
+    var  sample_Output= req.sample_Output;
+
+    // Form Validator
+    req.checkBody('ques_label','Name field is required').notEmpty();
+    req.checkBody('statement','Email field is required').notEmpty();
+    req.checkBody('constraint','Email is not valid').notEmpty();
+    req.checkBody('sample_Input','Username field is required').notEmpty();
+    req.checkBody('sample_Output','Password field is required').notEmpty();
 
 
+    var errors = req.validationErrors();
+    if(errors){
+
+    }else{
+        var new_Problem = new Problem({
+            ques_label:ques_label,
+            statement:statement,
+            constraint: constraint,
+            input_Format: input_Format,
+            output_Format: output_Format,
+            sample_Input: sample_Input,
+            sample_Output: sample_Output
+        })
+        Problem.createProblem(new_Problem,function(err, problem){
+            if(err)
+                throw err;
+            console.log(problem);
+        })
+
+        res.location('/problem/addcodingproblem');
+        res.redirect('/problem/addcodingproblem')
+    }
+
+    console.log(ques_label+" "+statement+" "+constraint+" "+input_Format+" "+ output_Format+" "+sample_Input+" "+sample_Output);
     res.render('addcodingproblem');
 });
 
@@ -24,12 +62,6 @@ router.post('/addcodingproblem', function(req, res, next) {
 router.get('/:tag/:id',function (req,res) {
     var tag=req.params.tag;
     var id=req.params.id;
-
-
-
-
-
-
     res.render('problem.ejs',
 
 
@@ -77,7 +109,7 @@ router.get('/:tag/:id',function (req,res) {
         }
 
 
-        );
+    );
 })
 
 module.exports=router;
